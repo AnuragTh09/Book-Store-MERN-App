7e160251a9d3fafc3badf965a0eac472a8f89947
AnuragTh09 <thakuranurag432@gmail.com>
2024-03-17 16:00:20 +0530

    CORS Policy Methods

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,22 @@
 import express from 'express';
 import bookRoute  from './routes/book.routes.js'
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}))
+
+// Middleware for handling CORS POLICY
+// method 1:- allow all origins with default of cors(*)
+app.use(cors())
+
+//method 2:- allow custom origins
+
+cors({
+    origin: 'http://localhost:5000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+})
 
 //routes
 
